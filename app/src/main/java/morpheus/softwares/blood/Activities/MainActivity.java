80eaa003ce21d3e95dee73ca4b29f3874f21c2c4
mopheshi <ndachimya@gmail.com>
2023-06-04 18:54:37 +0100

    Initial commit

---
+++ app/src/main/java/morpheus/softwares/blood/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package morpheus.softwares.blood.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import morpheus.softwares.blood.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/morpheus/softwares/blood/Activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package morpheus.softwares.blood.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import morpheus.softwares.blood.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
