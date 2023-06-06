b9beb681a2d8549e52f3244ae3dbcfe518a2f542
mopheshi <ndachimya@gmail.com>
2023-06-06 21:54:50 +0100

    ...UI Sign Up and Log In...

--- app/src/main/java/morpheus/softwares/blood/Activities/MainActivity.java
+++ app/src/main/java/morpheus/softwares/blood/Activities/MainActivity.java
@@ -2,18 +2,28 @@ package morpheus.softwares.blood.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import morpheus.softwares.blood.R;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView icon;
+    EditText search;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
 
     FirebaseAuth mAuth;
     FirebaseUser user;
@@ -23,7 +33,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        icon = findViewById(R.id.loginIcon);
+        search = findViewById(R.id.homeSearchView);
+        drawerLayout = findViewById(R.id.mainDrawer);
+        navigationView = findViewById(R.id.mainNavigator);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        toolbar = findViewById(R.id.homeToolbar);
+
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
@@ -32,15 +49,50 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
-        icon.setOnClickListener(v -> {
-            // Signs out the current user and clears it from the disk cache.
-            FirebaseAuth.getInstance().signOut();
+        search.setOnEditorActionListener((v, actionId, event) -> {
+            boolean handled = false;
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                Toast.makeText(MainActivity.this,
+                        "Searching for " + String.valueOf(search.getText()).trim() + "...",
+                        Toast.LENGTH_LONG).show();
+                handled = true;
+            }
+            return handled;
+        });
 
-            startActivity(new Intent(MainActivity.this, LogInActivity.class));
-            finish();
+        navigationView.setNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.createProfile)
+                Toast.makeText(MainActivity.this, "Create profile", Toast.LENGTH_LONG).show();
+            else if (item.getItemId() == R.id.beADonor)
+                Toast.makeText(MainActivity.this, "Be a donor", Toast.LENGTH_LONG).show();
+            else if (item.getItemId() == R.id.findDonor)
+                Toast.makeText(MainActivity.this, "Find a donor", Toast.LENGTH_LONG).show();
+            else if (item.getItemId() == R.id.exit) finishAffinity();
 
-            Toast.makeText(MainActivity.this, "Sign out successful!",
-                    Toast.LENGTH_LONG).show();
+//            switch (item.getItemId()) {
+//                case R.id.createProfile ->
+//                        Toast.makeText(MainActivity.this, "Create profile", Toast.LENGTH_LONG).show();
+//                case R.id.beADonor ->
+//                        Toast.makeText(MainActivity.this, "Be a donor", Toast.LENGTH_LONG).show();
+//                case R.id.findDonor ->
+//                        Toast.makeText(MainActivity.this, "Find a donor", Toast.LENGTH_LONG).show();
+//                case R.id.exit -> finishAffinity();
+//                default -> {
+//                }
+//            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return false;
         });
     }
 }
+
+//        icon.setOnClickListener(v -> {
+//            // Signs out the current user and clears it from the disk cache.
+//            FirebaseAuth.getInstance().signOut();
+//
+//            startActivity(new Intent(MainActivity.this, LogInActivity.class));
+//            finish();
+//
+//            Toast.makeText(MainActivity.this, "Sign out successful!",
+//                    Toast.LENGTH_LONG).show();
+//        });
